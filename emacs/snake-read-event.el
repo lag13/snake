81c72d7567a705c8297e832090485e2e796f3b7d
Lucas Groenendaal <groenendaal92@gmail.com>
2017-07-14 10:06:29 -0500

    Emacs (#2)

---
+++ emacs/snake-read-event.el
@@ -0,0 +1,103 @@
+;;; snake-read-event.el --- Implementation of Snake for Emacs
+
+;; Author: Lucas Groenendaal
+
+;;; Commentary:
+
+;; It's worth noting that emacs already has a built in snake game (of
+;; course it does), I just felt like making my own. From my
+;; experimentation with making games/animations I've learned a couple
+;; things:
+
+;; 1. When elisp code is being executed any keys you press are ignored
+;; unless you explicitly read them in your code. One exception to this
+;; is C-g which will stop any executing lisp code.
+
+;; 2. When elisp code is being executed the buffer does NOT get
+;; redrawn unless you explicitly tell it to.
+
+;; After a bit of research, it seems there are two (although there are
+;; probably more) approaches you can take when making games. I believe
+;; the first one is preferred. This file does (2).
+
+;; 1. Create key mappings to control the game state. If the game can
+;; update itself without human input (like with snake) then use the
+;; "run-with-timer" function to execute the game loop in the
+;; background. One thing I'm a little unsure about is if there is the
+;; potential for race conditions here since different key presses
+;; could all be changing some global structure.
+
+;; 2. Write code to read input yourself:
+;; http://dantorop.info/project/emacs-animation/lisp8.html. The
+;; downside with this approach over the other is that since your game
+;; is reading all input, you can't execute key bindings like C-x C-o
+;; to change windows. So once you start the game then you pretty much
+;; must finish it. I'm also not sure how to read input asynchronously
+;; so it does not effect the speed of the game.
+
+;; Here are some functions+variables which seem useful for
+;; games+animations with emacs:
+
+;; sit-for: sleeps for some amount of time or until there is input
+;; available, whichever comes first, and redraws the screen. As I said
+;; earlier, emacs does not consume input while executing a function,
+;; unless you do it yourself, meaning that if you have a loop with a
+;; "sit-for" inside then when you press a character the "sit-for" will
+;; not sleep anymore since there is a character in the input buffer.
+;; So the loop will finish as fast as your computer can process it.
+;; I'm not entirely sure of the utility of that "feature".
+
+;; sleep-for: sleeps for some amount of time. "sit-for" uses it
+;; internally.
+
+;; redisplay: redraws the screen. "sit-for" uses this internally.
+
+;; cursor-type: setting this to nil will hide the cursor.
+
+;; read-event: reads a keypress from the keyboard. It's not documented
+;; but it also appears to redraw the buffer.
+
+;; buffer-disable-undo: disables undo in the current buffer. Since
+;; games can produce a lot of "frames" it means all those frames
+;; wouldn't be stored in emacs' memory.
+
+(load-file "util.el")
+
+;; I originally said it in the go implementation but I've added one
+;; more note. I think the best structure for a game loop is:
+;; 1. Render (the initial game state should be drawn after all)
+;; 2. Sleep (gives the player time to see the game and respond to it's current state
+;; 3. Get input
+;; 4. Update the game state
+;; 5. After the game loop exits (presumably due to a win or loss) redraw the game so the "end" state of the game can be seen
+(defun lag13/snake-read-event ()
+  "Starts the game of snake."
+  (interactive)
+  (switch-to-buffer "*lag13/snake-read-event*")
+  (let* ((cursor-type nil)
+         (width 20)
+         (height 20)
+         (snake-body '((2 . 2) (2 . 1) (1 . 1) (1 . 2)))
+         (direction '(0 . 1))
+         (food-pos (lag13/gen-food-position width height snake-body))
+         (score 0)
+         (is-paused nil))
+    (while (not (lag13/snake-game-is-over width height snake-body))
+      (lag13/snake-draw-game width height snake-body food-pos score is-paused)
+      (let ((key (read-event nil nil 0.2)))
+        (cond
+         ((eq key 'up) (setq direction '(0 . -1)))
+         ((eq key 'down) (setq direction '(0 . 1)))
+         ((eq key 'left) (setq direction '(-1 . 0)))
+         ((eq key 'right) (setq direction '(1 . 0)))
+         ((eq key ?p) (setq is-paused (not is-paused)))))
+      (unless is-paused
+        (let ((snake-new-head (lag13/add-cons-pair direction (car snake-body))))
+          (cond
+           ((equal food-pos snake-new-head)
+            (setq score (1+ score))
+            (setq snake-body (cons snake-new-head snake-body))
+            (setq food-pos (lag13/gen-food-position width height snake-body)))
+           (t
+            (setq snake-body (cons snake-new-head (butlast snake-body))))))))
+    (lag13/snake-draw-game width height snake-body food-pos score nil)))
---
+++ emacs/snake.el
@@ -0,0 +1,161 @@
+;;; snake.el --- Implementation of Snake for Emacs
+
+;; Author: Lucas Groenendaal
+
+;;; Commentary:
+
+;; A different implementation of snake where the key presses are
+;; defined as key mappings.
+
+(load-file "util.el")
+
+(defconst *lag13/snake-board-width* 20
+  "The width of the snake game.")
+
+(defconst *lag13/snake-board-height* 20
+  "The height of the snake game.")
+
+(defvar *lag13/snake-direction* nil
+  "The direction the snake is traveling in.")
+
+(defvar *lag13/snake-direction-queue* nil
+  "A queue of directions which have been inputted. The purpose of having this is so that if a player quickly presses something like \"up\" then \"left\", both keystrokes will be recored")
+
+(defvar *lag13/snake-body* nil
+  "The entire body of the snake.")
+
+(defvar *lag13/snake-food-pos* nil
+  "Where the food is.")
+
+(defvar *lag13/snake-score* nil
+  "What the current score is.")
+
+(defvar *lag13/snake-is-paused* nil
+  "Whether the game is paused or not.")
+
+(defvar *lag13/snake-timer* nil
+  "References the timer which runs the game loop.")
+
+(defun lag13/snake-previous-entered-direction ()
+  "Returns the previously entered direction."
+  (if *lag13/snake-direction-queue*
+      (car *lag13/snake-direction-queue*)
+    *lag13/snake-direction*))
+
+(defun lag13/snake-was-vertical-dir ()
+  "Returns t if the previously entered direction was vertical."
+  (zerop (car (lag13/snake-previous-entered-direction))))
+
+(defun lag13/snake-move-left ()
+  "Moves the snake to the left."
+  (interactive)
+  (when (lag13/snake-was-vertical-dir)
+    (push '(-1 . 0) *lag13/snake-direction-queue*)))
+
+(defun lag13/snake-move-right ()
+  "Moves the snake to the right."
+  (interactive)
+  (when (lag13/snake-was-vertical-dir)
+    (push '(1 . 0) *lag13/snake-direction-queue*)))
+
+(defun lag13/snake-was-horizontal-dir ()
+  "Returns t if the previously entered direction was horizontal."
+  (zerop (cdr (lag13/snake-previous-entered-direction))))
+
+(defun lag13/snake-move-up ()
+  "Moves the snake to the up."
+  (interactive)
+  (when (lag13/snake-was-horizontal-dir)
+    (push '(0 . -1) *lag13/snake-direction-queue*)))
+
+(defun lag13/snake-move-down ()
+  "Moves the snake to the down."
+  (interactive)
+  (when (lag13/snake-was-horizontal-dir)
+    (push '(0 . 1) *lag13/snake-direction-queue*)))
+
+(defun lag13/snake-pause ()
+  "Pauses the game."
+  (interactive)
+  (setq *lag13/snake-is-paused* (not *lag13/snake-is-paused*)))
+
+;; As of Emacs 25.1, all special-mode really does is set
+;; buffer-read-only so that the buffer cannot be edited. This feature
+;; is useful for games.
+(define-derived-mode lag13/snake-mode special-mode "lag13/Snake"
+  "A mode for playing Snake."
+  (setq cursor-type nil)
+  ;; When I was working on this game I made the mistake of assuming
+  ;; that by default add-hook would create a hook local to the snake
+  ;; buffer. I was very wrong and it lead to a lot of headache. By
+  ;; default, add-hook modifies the global hook. So what was happening
+  ;; was that EVERY time a buffer was killed the "timer cancel"
+  ;; function was called. From first hand experience it turns out that
+  ;; emacs kills a LOT of buffers behind the scenes so this function
+  ;; was triggering a lot and failing and causing general weirdness
+  ;; (probably in part because the timer variable still held a
+  ;; reference to the cancelled timer). That last t paramter makes
+  ;; this hook addition only apply in the "snake" buffer.
+  (add-hook 'kill-buffer-hook 'lag13/snake-cancel-timer nil t)
+  (define-key lag13/snake-mode-map (kbd "<left>") 'lag13/snake-move-left)
+  (define-key lag13/snake-mode-map (kbd "<right>") 'lag13/snake-move-right)
+  (define-key lag13/snake-mode-map (kbd "<up>") 'lag13/snake-move-up)
+  (define-key lag13/snake-mode-map (kbd "<down>") 'lag13/snake-move-down)
+  (define-key lag13/snake-mode-map (kbd "p") 'lag13/snake-pause)
+  (define-key lag13/snake-mode-map (kbd "n") 'lag13/snake))
+
+(defun lag13/get-direction ()
+  "Set's the current direction that the snake is traveling in."
+  (if *lag13/snake-direction-queue*
+      (let ((new-dir (car (last *lag13/snake-direction-queue*))))
+        (setq *lag13/snake-direction-queue* (butlast *lag13/snake-direction-queue*))
+        new-dir)
+    *lag13/snake-direction*))
+
+(defun lag13/snake-update (snake-buffer)
+  "Updates the snake game state and draws it."
+  (when (and (not *lag13/snake-is-paused*)
+             (eq (current-buffer) snake-buffer))
+    (if (lag13/snake-game-is-over *lag13/snake-board-width* *lag13/snake-board-height* *lag13/snake-body*)
+	(lag13/snake-cancel-timer)
+      (setq *lag13/snake-direction* (lag13/get-direction))
+      (let ((snake-new-head (lag13/add-cons-pair *lag13/snake-direction* (car *lag13/snake-body*))))
+        (cond
+         ((equal *lag13/snake-food-pos* snake-new-head)
+          (setq *lag13/snake-score* (1+ *lag13/snake-score*))
+          (setq *lag13/snake-body* (cons snake-new-head *lag13/snake-body*))
+          (setq *lag13/snake-food-pos* (lag13/gen-food-position *lag13/snake-board-width* *lag13/snake-board-height* *lag13/snake-body*)))
+         (t
+          (setq *lag13/snake-body* (cons snake-new-head (butlast *lag13/snake-body*))))))))
+  (when (eq (current-buffer) snake-buffer)
+    (lag13/snake-draw-game *lag13/snake-board-width* *lag13/snake-board-height* *lag13/snake-body* *lag13/snake-food-pos* *lag13/snake-score* *lag13/snake-is-paused*)))
+
+(defun lag13/snake-init ()
+  "Initializes the game of snake."
+  ;; The game function could be invoked in the middle of another game
+  ;; so we need to clean up this timer, otherwise it will generate
+  ;; another one (which makes the game go twice as fast haha).
+  (lag13/snake-cancel-timer)
+  (setq *lag13/snake-body* '((2 . 2) (2 . 1) (1 . 1) (1 . 2)))
+  (setq *lag13/snake-direction* '(0 . 1))
+  (setq *lag13/snake-direction-queue* nil)
+  (setq *lag13/snake-food-pos* (lag13/gen-food-position *lag13/snake-board-width* *lag13/snake-board-height* *lag13/snake-body*))
+  (setq *lag13/snake-score* 0)
+  (setq *lag13/snake-is-paused* nil)
+  (lag13/snake-draw-game *lag13/snake-board-width* *lag13/snake-board-height* *lag13/snake-body* *lag13/snake-food-pos* *lag13/snake-score* *lag13/snake-is-paused*)
+  (setq *lag13/snake-timer* (run-at-time 0.1 0.1 'lag13/snake-update (current-buffer))))
+
+(defun lag13/snake-cancel-timer ()
+  "Cancels the timer driving the the game."
+  (when *lag13/snake-timer*
+    (cancel-timer *lag13/snake-timer*)
+    ;; Just good hygine to not leave any references we don't need
+    ;; anymore.
+    (setq *lag13/snake-timer* nil)))
+
+(defun lag13/snake ()
+  "Starts the game Snake."
+  (interactive)
+  (switch-to-buffer "*lag13/snake*")
+  (lag13/snake-mode)
+  (lag13/snake-init))
