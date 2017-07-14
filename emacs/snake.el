664c4752a9857d727ecd1baa966433873c3da744
Lucas Groenendaal <groenendaal92@gmail.com>
2017-07-13 23:10:41 -0500

    Add documentation

--- emacs/snake.el
+++ emacs/snake.el
@@ -1,4 +1,62 @@
-;;; snake.el --- Implementation of Snake for Emacs.
+;;; snake.el --- Implementation of Snake for Emacs
+
+;; Author: Lucas Groenendaal
+
+;;; Commentary:
+
+;; It's worth noting that emacs already has a built in snake game (of
+;; course it does), I just felt like making my own. From my
+;; experimentation with making games/animations I've learned a couple
+;; things:
+
+;; 1. When elisp code is being executed any keys you press are ignored
+;; unless you explicitly read them in your code. One exception to this
+;; is C-g which will stop any executing lisp code.
+
+;; 2. When elisp code is being executed the buffer does NOT get
+;; redrawn unless you explicitly tell it to.
+
+;; After a bit of research, it seems there are two (although there are
+;; probably more) approaches you can take when making games. I believe
+;; the first one is preferred.
+
+;; 1. Create key mappings to control the game state. If the game can
+;; update itselft without human input (like with snake) then use the
+;; "run-with-timer" function to execute the game loop in the
+;; background.
+
+;; 2. Write code to read input yourself:
+;; http://dantorop.info/project/emacs-animation/lisp8.html. The
+;; downside with this approach over the other is that since your game
+;; is reading all input, you can't execute key bindings like C-x C-o
+;; to change windows. So once you start the game then you pretty much
+;; must finish it.
+
+;; Here are some functions+variables which seem useful for animation
+;; with emacs:
+
+;; sit-for: sleeps for some amount of time or until there is input
+;; available, whichever comes first, and redraws the screen. As I said
+;; earlier, emacs does not consume input while executing a function,
+;; unless you do it yourself, meaning that if you have a loop with a
+;; "sit-for" inside then when you press a character the "sit-for" will
+;; not sleep anymore since there is a character in the input buffer.
+;; So the loop will finish as fast as your computer can process it.
+;; I'm not entirely sure of the utility of that "feature".
+
+;; sleep-for: sleeps for some amount of time. "sit-for" uses it
+;; internally.
+
+;; redisplay: redraws the screen. "sit-for" uses this internally.
+
+;; cursor-type: setting this to nil will hide the cursor.
+
+;; read-event: reads a keypress from the keyboard. It's not documented
+;; but it also appears to redraw the buffer.
+
+;; buffer-disable-undo: disables undo in the current buffer. Since
+;; games can produce a lot of "frames" it means all those frames
+;; wouldn't be stored in emacs' memory.
 
 (defun lag13/intersperse (i lst)
   "Intersperse a value I into the list LST."
@@ -125,5 +183,6 @@ character is drawn if it is off of the grid."
     (lag13/snake-draw-game width height snake-body food-pos score nil)
     (goto-char (point-max))
     (newline)
-    (when (lag13/snake-won width height snake-body)
-      (insert "Holy shit you won!!"))))
+    (if (lag13/snake-won width height snake-body)
+        (insert "Holy shit you won!!")
+      (insert "You lost. Then again one usually \"loses\" at snake"))))
