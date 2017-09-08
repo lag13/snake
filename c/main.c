92c62538f194086f32778baf07231bf504838e6c
Lucas Groenendaal <groenendaal92@gmail.com>
2017-09-08 15:55:13 -0500

    save work

--- c/main.c
+++ c/main.c
@@ -78,11 +78,31 @@ void drawGameOverText(int height, bool wonGame) {
   mvprintw(height+1, 0, s);
 }
 
+// TODO: Read about threads and if I need to cancel this thread or if
+// it will stop automatically when main does I think it will stop
+// automatically. But I definitely want to learn more about
+// threads/processes and depending on that research I'd like to remove
+// these 2 global variables and instead pass them into the functions
+// that need them. I'll need to use a closure for the render function.
+// It would be cool if I could implement the equivalent of goroutines
+// in C. I wonder if that would be hard or if that would have benefits
+// in terms of memory and such (and how would I even measure those
+// sorts of things).
+
 // It turns out that ncurses is not thread safe so we need to have
 // this mutex to prevent multiple threads from executing nucurses
 // functions at the same time:
 // http://www.linuxmisc.com/9-unix-programmer/87e7383a80449bf7.htm
 pthread_mutex_t ncursesMu;
+// I wanted to make this variable local to main and pass it into the
+// getInput function but apparently that *could* lead to issues where:
+// 1. main terminates so any variables it declared are invalid 2.
+// *right* after main terminates, the thread is scheduled and since it
+// references this now invalid variable it would fail. I've never seen
+// this happen and for such a dinky program I d
+// https://stackoverflow.com/questions/16230542/passing-multiple-arguments-to-threaded-function-from-pthread-create.
+// https://en.wikipedia.org/wiki/Thread_(computing)
+playeractionQueue *queue;
 
 void render(int width, int height, posList snake, pos food, bool paused, int score, bool gameIsWon, bool gameIsLost) {
   pthread_mutex_lock(&ncursesMu);
@@ -95,15 +115,15 @@ void render(int width, int height, posList snake, pos food, bool paused, int sco
   if (gameIsWon || gameIsLost) {
     drawGameOverText(height, gameIsWon);
   }
-  // TODO: Display the controls. To do this we need to create a hash
-  // table of char -> playeraction.
+  // TODO: Display the controls. To do this we'll need to create a
+  // hash table of char -> playeraction. I would also like to
+  // investigate how one could do interface like stuff in C as really
+  // this mapping should be something like (Show a) => a ->
+  // playeraction -> string. I'll put it in this file for now but I
+  // guess it should probably go inside the snake.h module.
   refresh();
   pthread_mutex_unlock(&ncursesMu);
 }
-
-// TODO: Pass this variable into the getInput function instead of
-// having it be a global variable.
-playeractionQueue *queue;
 
 void *getInput() {
   while (1) {
@@ -162,6 +182,7 @@ int main(int argc, char** argv) {
     queue = &(playeractionQueue) { .arr = (mem+snakeSpaceOccupies), .mu = &mu };
   }
 
+  pthread_t threadID;
   {
     // initializes ncurses:
     // http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/
@@ -174,15 +195,21 @@ int main(int argc, char** argv) {
     noecho();
     // hides the cursor
     curs_set(0);
-    // so calls to getch() do not block
+    // The calls to getch() happen in another thread but since all
+    // ncurses operations are protected by a mutex the getch()
+    // function cannot block because that would prevent other ncurses
+    // functions from running.
     timeout(0);
     must_pthread_mutex_init(&ncursesMu, NULL);
-    pthread_t threadID;
     must_pthread_create(&threadID, NULL, &getInput, NULL);
   }
 
   snake(width, height, mem, queue, render);
-
+  /* pthread_join(threadID, NULL); */
+  // TODO: Maybe register some trap handler things if the user presses
+  // ctrl-c? I wonder if ncurses does this so that endwin() always
+  // gets closed.
   endwin();
   free(mem);
+  return 0;
 }
