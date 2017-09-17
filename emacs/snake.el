54de4ec53cb2f9db93488c44ac922d5cb4cc764a
Lucas Groenendaal <groenendaal92@gmail.com>
2017-09-17 14:00:01 -0500

    update readme

--- emacs/snake.el
+++ emacs/snake.el
@@ -8,6 +8,8 @@
 ;; defined as key mappings which I believe is the preferred way of
 ;; making games (I like it more as well).
 
+;; TODO: Have this file be loaded on emacs startup.
+
 (load-file "util.el")
 
 (defconst *lag13/snake-board-width* 15
@@ -89,8 +91,8 @@ is valid."
   ;; emacs kills a LOT of buffers behind the scenes so this function
   ;; was triggering a lot and failing and causing general weirdness
   ;; (probably in part because the timer variable still held a
-  ;; reference to the cancelled timer). That last t paramter makes
-  ;; this hook addition only apply in the "snake" buffer.
+  ;; reference to the cancelled timer). That last paramter makes this
+  ;; hook addition only apply in the "snake" buffer.
   (add-hook 'kill-buffer-hook 'lag13/snake-cancel-timer nil t)
   (define-key lag13/snake-mode-map (kbd "a") 'lag13/snake-move-left)
   (define-key lag13/snake-mode-map (kbd "d") 'lag13/snake-move-right)
